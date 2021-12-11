2e50fc66eaf4649615ab9da70c9c6ab761d69444
Nikita.Melnyk <Nikita.Melnyk@dreamscapenetworks.com>
2021-12-12 01:57:42 +0200

    12/12/2021

--- src/index.js
+++ src/index.js
@@ -2,12 +2,42 @@ import _ from 'lodash'
 import $ from '../node_modules/jquery/dist/jquery.js'
 
 
-let array = [1,2,3,4,5]
 
-_.fill(array, 'BBB')
-console.log(array)
-console.log(1)
-console.log('width: ', $(window).width())
-console.log('height: ', $(window).height())
+let hero = {
+  width: 65,
+  height: 65
+}
 
-console.log(12)
+//Mouse move event
+$(window).mousemove(function(event) {
+  //move hero
+  $("#hero").css("left", event.pageX - hero.width/2)
+  $("#hero").css("top", event.pageY - hero.height/2)
+});
+//Mouse click event
+$(window).click(function(event){
+  console.log('clicked')
+})
+
+
+//1536 - 23.63
+//739.2 - 11.37
+
+
+
+
+function main_timer(){
+  let delta = 15
+  setInterval(function(){
+    console.log('timer')
+    //get old coordinates
+    let oldy = $('.block2').position().top
+    console.log(oldy)
+    //delta coordinates
+    let newy = oldy + delta
+    console.log('new: ', newy)
+    //set new coordinates
+    $('.block2').css("top", newy)
+  }, 2000)
+}
+main_timer()
