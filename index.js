9bc6b0eb033997a5cc22be8a99e8d7bc3323e845
Nikita.Melnyk <Nikita.Melnyk@dreamscapenetworks.com>
2021-12-12 00:53:27 +0200

    initial state 1

--- index.js
+++
@@ -1,4 +0,0 @@
-import {person1} from './Post.js'
-
-
-person1.say_hello()
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash'
+import $ from '../node_modules/jquery/dist/jquery.js'
+
+
+let array = [1,2,3,4,5]
+
+_.fill(array, 'BBB')
+console.log(array)
+console.log(1)
+console.log('width: ', $(window).width())
+console.log('height: ', $(window).height())
+
+console.log(12)
