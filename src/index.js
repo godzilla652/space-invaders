91b258c5aa7e431648aa19e6119ea902bc0ba0dc
Nikita.Melnyk <Nikita.Melnyk@dreamscapenetworks.com>
2021-12-12 02:47:29 +0200

    webpack configuration basic template for vanilla js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 import $ from '../node_modules/jquery/dist/jquery.js'
-
+import './styles/style.css' 
 
 console.log('test')
 let hero = {
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,20 +3,28 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: {
+  mode: 'development',
+  entry: {
       main: './src/index.js',
-//      analytics: ''./src/analytics.js''
+      //analytics: ''/analytics.js''
     },
-    mode: 'development',
 
-    output: {
+  output: {
       filename: '[name].[contenthash].js',
       path: path.resolve(__dirname, 'dist' )
     },
-    plugins: [
+  plugins: [
       new HTMLWebpackPlugin({
         template: './src/index.html'
       }),
       new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
+  }
 }
